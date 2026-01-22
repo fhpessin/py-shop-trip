6e1600350612bc95219513a60220a90fad78533d
Felipe Pessin <fhpessin@gmail.com>
2026-01-22 14:10:18 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import json
 import os
-from app.helpers import Customer, Shop
+from app.customer import Customer
+from app.shop import Shop
 
 
 def shop_trip() -> None:
@@ -21,19 +22,22 @@ def shop_trip() -> None:
         for shop in shops:
             total_cost = customer.get_trip_cost(shop, fuel_price)
             print(f"{customer.name}'s trip to the {shop.name} "
-                  f"costs {total_cost}")
+                  f"costs {total_cost:.2f}")
             if total_cost < min_total_cost:
                 min_total_cost = total_cost
                 best_shop = shop
 
         if best_shop and min_total_cost <= customer.money:
             print(f"{customer.name} rides to {best_shop.name}\n")
-            best_shop.print_receipt(customer.name, customer.product_cart)
-            customer.money -= min_total_cost
+            
+            # ATUALIZAÇÃO: Localização muda ANTES do recibo
             customer.location = best_shop.location
+            best_shop.print_receipt(customer.name, customer.product_cart)
+            
+            customer.money -= min_total_cost
             print(f"\n{customer.name} rides home")
             print(f"{customer.name} now has "
-                  f"{round(customer.money, 2)} dollars\n")
+                  f"{customer.money:.2f} dollars\n")
         else:
             print(f"{customer.name} doesn't have enough money to make a "
                   "purchase in any shop\n")
